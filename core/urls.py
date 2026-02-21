8ad908b8d007bfffdcfdfb157ce789604ed15bae
Viny Ferrari <vinybag@gmail.com>
2026-02-21 01:45:13 -0300

    Remove: View temporária de criação de admin (PostgreSQL configurado)

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
-from .criar_admin_view import criar_admin_secreto
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('sobre/', views.sobre, name='sobre'),
-    path('criar-admin-secreto/', criar_admin_secreto),
 ]
