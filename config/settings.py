192ec21358fda28593bda8fc232f6c78903eb55e
Edson Vinícius Ferrari <vinybag@gmail.com>
2026-04-09 10:31:53 -0300

    Fix: Serve media files in production

--- config/settings.py
+++ config/settings.py
@@ -168,3 +168,8 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='BAILAH <noreply@bailah.com>')
+
+if not DEBUG:
+    import os
+    from django.conf.urls.static import static
+    from django.urls import path, include
