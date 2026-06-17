4d7e64835f635def8eef787947bf2ebcd6cbf606
Edson Vinícius Ferrari <vinybag@gmail.com>
2026-06-17 11:32:04 -0300

    teste de imagem novamente

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,4 @@ urlpatterns = [
     path('dashboard/', redirecionar_dashboard, name='redirecionar_dashboard'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
